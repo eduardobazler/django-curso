2edf3c799038d71eef134e5f5e4ea1ddebd4db8f
eduardo <eduardo.bazler@unesp.br>
2021-05-10 12:03:03 -0300

    debug tool bar

--- pypro/urls.py
+++ pypro/urls.py
@@ -23,3 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home)
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns.append(
+        path('__debug__/', include(debug_toolbar.urls))
+    )
