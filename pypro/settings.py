4e98984442a03fb17ff1c9e5d9a34bade472d492
eduardo <eduardo.bazler@unesp.br>
2021-05-20 21:31:07 -0300

    video incorporado

--- pypro/settings.py
+++ pypro/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'collectfast',
     'django.contrib.staticfiles',
-    'pypro.base'
+    'pypro.base',
+    'pypro.aperitivos'
 ]
 
 MIDDLEWARE = [
